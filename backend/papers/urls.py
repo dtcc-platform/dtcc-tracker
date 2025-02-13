77ec4c2ac7899e7e072bdc78abc5cb35aaf74aef
Themistoklis Arvanitis <themistoklisarv2001@gmail.com>
2025-02-13 10:12:13 +0100

    Added additionalAuthors Handle. Only Charfield In Model

--- backend/papers/urls.py
+++ backend/papers/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('papers/', PaperListCreateView.as_view(), name='paper-list-create'),
-    path('papers/delete/<str:doi>/', PaperDeleteView.as_view(), name='paper-delete'),
-    path('papers/update/<str:doi>/', PaperUpdateView.as_view(), name='paper-update'),  # PUT Route
+    path('papers/delete/<path:doi>/', PaperDeleteView.as_view(), name='paper-delete'),
+    path('papers/update/<path:doi>/', PaperUpdateView.as_view(), name='paper-update'),  # PUT Route
 
     path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
-    path('projects/delete/<str:doi>/', ProjectDeleteView.as_view(), name='project-delete'),
-    path('projects/update/<str:doi>/', ProjectUpdateView.as_view(), name='project-update'),  # PUT Route
+    path('projects/delete/<path:doi>/', ProjectDeleteView.as_view(), name='project-delete'),
+    path('projects/update/<path:doi>/', ProjectUpdateView.as_view(), name='project-update'),  # PUT Route
 
     path('doi-info/', DOIInfoView.as_view(), name='doi-info'),
 ]
