e9300a6a4b8ec6ac104c33d9aa0a8780af9ada74
Themistoklis Arvanitis <themistoklisarv2001@gmail.com>
2025-09-25 13:05:21 +0200

    Fixed build error caused by linting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,25 +1,21 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 import path from 'path';
 import dotenv from 'dotenv';
 
-// Load the .env file from the repo root (tracker/.env)
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-const config: NextConfig = {
-  // Make sure to prefix any variable that needs to be exposed to the client with NEXT_PUBLIC_
+const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-  // Other Next.js config options can go here...
+
   eslint: {
-    ignoreDuringBuilds: true,
-  }
-};
-// next.config.js
-module.exports = {
+    ignoreDuringBuilds: true, 
+  },
+
   basePath: '/tracker',
   assetPrefix: '/tracker',
   trailingSlash: true,
-}
+};
 
-export default config;
+export default nextConfig;
